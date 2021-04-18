2018dc27308cdcf12e54eb01f1361eccacb4e63e
inunosinsi <tsuyoshi@saitodev.co>
2021-04-19 06:37:20 +0900

    [Refactoring]

--- function/function.soy2_setcookie.php
+++ function/function.soy2_setcookie.php
@@ -1,20 +1,19 @@
 <?php
-
 /*
  * PHPのバージョンによってsetcookieのオプションの値を変える
  */
- function soy2_setcookie($key, $value=null, $opts=array()){
- 	if(!isset($opts["expires"]) || !is_numeric($opts["expires"])) $opts["expires"] = time()-1;
- 	if(!isset($opts["secure"]) || !is_bool($opts["secure"])) $opts["secure"] = false;
+function soy2_setcookie($key, $value=null, $opts=array()){
+	if(!isset($opts["path"]) || !is_string($opts["path"]) || !strlen($opts["path"])) $opts["path"] = "";
+	if(!isset($opts["domain"]) || !is_string($opts["domain"]) || !strlen($opts["domain"])) $opts["domain"] = null;
+	if(!isset($opts["expires"]) || !is_numeric($opts["expires"])) $opts["expires"] = time()-1;
+	if(!isset($opts["httponly"]) || !is_bool($opts["httponly"])) $opts["httponly"] = true;
+	if(!isset($opts["secure"]) || !is_bool($opts["secure"])) $opts["secure"] = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on");
 
- 	$vArr = explode(".", phpversion());
- 	if(($vArr[0] >= 8 || ($vArr[0] >= 7 && $vArr[1] >= 3))){	//php 7.3以降 samesiteの指定が出来る
- 		if(!isset($opts["samesite"])) $opts["samesite"] = ($opts["secure"]) ? "Lax" : "None";
- 		setcookie($key, $value, $opts);
- 	}else{
- 		$path = (isset($opts["path"])) ? $opts["path"] : "";
- 		$domain = (isset($opts["domain"])) ? $opts["domain"] : "";
- 		$httponly = (isset($opts["httponly"]) && is_bool($opts["httponly"])) ? $opts["httponly"] : false;
- 		setcookie($key, $value , $opts["expires"], $path, $domain, $opts["secure"], $httponly);
- 	}
- }
+	$vArr = explode(".", phpversion());
+	if(($vArr[0] >= 8 || ($vArr[0] >= 7 && $vArr[1] >= 3))){	//php 7.3以降 samesiteの指定が出来る
+		if(!isset($opts["samesite"])) $opts["samesite"] = "Lax";	//常にLaxにする
+		setcookie($key, $value, $opts);
+	}else{
+		setcookie($key, $value , $opts["expires"], $opts["path"], $opts["domain"], $opts["secure"], $opts["httponly"]);
+	}
+}
