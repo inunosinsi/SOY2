86321f4a3072a40a65f0c94968387773d3ad8898
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2026-08-11 15:18:33 +0900

    Update soy2.php

--- lib/soy2.php
+++ lib/soy2.php
@@ -8882,7 +8882,24 @@ function soy2_serialize($var){
  * @param $string soy2_serializeの出力する文字列
  */
 function soy2_unserialize(string $string){
-	return (strlen($string)) ? unserialize(stripslashes($string)) : array();
+	// シリアル化されたオブジェクトを復元する時に、指定のオブジェクトでない場合は復元を禁止する
+	static $allowed;
+	if(is_null($allowed)){
+		/**
+		 * @usage 同一ディレクトリ内にallowedClasses.phpを作成
+		 * シリアライズ化したおオブジェクトのデータを復元する時に復元を許可するオブジェクト名を配列で指定
+		 * <?php
+		 * $allowed = array();
+		 */
+		$allowed = array();
+		if(file_exists(__DIR__."/allowedClasses.php")) include(__DIR__."/allowedClasses.php");
+		if(!is_array($allowed) || !count($allowed)) $allowed = false;
+	}
+	if(!strlen($string)) return array();
+	
+	return unserialize(stripslashes($string), array(
+		array("allowed_classes" => $allowed)
+	));
 }
 /**
  * soy2_scanfiles
